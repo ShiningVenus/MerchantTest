3bad89eb0912f4e63c1ff3bb6b550ad88fc99578
ShiningVenus <paperplane0424@gmail.com>
2022-01-06 17:24:28 -0500

    update1

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -13,7 +13,10 @@ const Shop = () => {
 
   const filterData = (data) => {
     let obj = {}
-    data.reverse().forEach(item => obj[item.id] = item)
+    data.reverse().forEach(item => {
+      if(obj[item.id] == null || obj[item.id] === 'coming_soon')
+        obj[item.id] = item
+    })
     const resArr = Object.values(obj)
   
     resArr.sort((a, b) => {
